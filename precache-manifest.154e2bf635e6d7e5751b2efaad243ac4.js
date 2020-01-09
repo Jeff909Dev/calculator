de5d02f83ee76192ef2afeb111ac8193381be7e8
Jeffrey Viveros <temel57@grupotemel.net>
2020-01-09 12:36:51 +0100

    Updates

---
+++ precache-manifest.154e2bf635e6d7e5751b2efaad243ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d52784a5800e2ed72b25f7f970d05d5",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "49cc690a2c714cb6679e",
+    "url": "/calculator/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "cb7055cb4b691a8299a1",
+    "url": "/calculator/static/css/main.366eb034.chunk.css"
+  },
+  {
+    "revision": "49cc690a2c714cb6679e",
+    "url": "/calculator/static/js/2.d0cb4d0d.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/calculator/static/js/2.d0cb4d0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb7055cb4b691a8299a1",
+    "url": "/calculator/static/js/main.3ab23d50.chunk.js"
+  },
+  {
+    "revision": "64f3c865b5c158de7188",
+    "url": "/calculator/static/js/runtime-main.8bbc4a42.js"
+  }
+]);
